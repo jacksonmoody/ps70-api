0c1c6f7f32a3ffcbef50b45b6cd4443dc3eb9e69
Jackson <jackson@jacksonmoody.com>
2023-03-28 16:34:53 -0400

    Updated Variables

--- index.js
+++ index.js
@@ -2,24 +2,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-let direction;
-let speed;
-
 app.get('/direction', (req, res) => {
-  res.send(direction);
+  res.send(app.get('direction'));
 })
 
 app.get('/speed', (req, res) => {
-    res.send(speed);
+    res.send(app.get('speed'));
   })
 
 app.post('/direction', (req, res) => {
-    direction = req.query.direction;
+  app.set('diretion', req.query.direction);
     res.send(`Set direction to ${direction}`);
 })
 
 app.post('/speed', (req, res) => {
-    speed = req.query.speed;
+    app.set('speed', req.query.speed);
     res.send(`Set speed to ${speed}`);
 })
 
