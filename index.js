fdb167467007c6ae88bb20140ec8c8a71496c038
Jackson <jackson@jacksonmoody.com>
2023-04-11 18:41:41 -0400

    Updated to be more general

--- index.js
+++ index.js
@@ -1,30 +1,16 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send(`The current direction is ${app.get('direction')} and the current speed is ${app.get('speed')}`);
+  res.json(app.get('data'));
 })
 
-app.get('/direction', (req, res) => {
-  res.json({ direction: app.get('direction'), time: app.get('time') })
-})
-
-app.get('/speed', (req, res) => {
-   res.send(app.get('speed'));
-  })
-
-app.post('/direction', (req, res) => {
-  console.log("New Direction Request");
-  app.set('direction', req.query.direction);
-  app.set('time', req.query.time);
-  res.send(`Set direction to ${app.get('direction')} and time to ${app.get('time')}`);
-})
-
-app.post('/speed', (req, res) => {
-  console.log("New Speed Request");
-  app.set('speed', req.query.speed);
-  res.send(`Set speed to ${app.get('speed')}`);
+app.post('/', (req, res) => {
+  console.log(req.body);
+  app.set('data', req.body);
+  res.send(`Set data to ${app.get('data')}`);
 })
 
 app.listen(port, () => {
